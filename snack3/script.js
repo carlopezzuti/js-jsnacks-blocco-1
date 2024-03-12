2b6bde94b74da649283b9694fb0ddbaa0ec365e3
carlopezzuti <carlopezzuti89@gmail.com>
2024-03-12 01:14:59 +0100

    end snack 3

---
+++ snack3/script.js
@@ -0,0 +1,13 @@
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+
+const numbersList = [];
+
+for(let i = 0; i < 6; i++) {
+    const userNumber = parseInt(prompt("Inserisci un numero"));
+
+if (userNumber %2 !== 0) {
+        numbersList.push(userNumber);
+}
+}
+
+console.log(numbersList);
